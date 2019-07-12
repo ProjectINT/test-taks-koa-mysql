1950a042cfc58a4b304b0c4f233d9a86d9dfa095
Yuri <admin@somnium.su>
2019-07-12 21:04:30 +0300

    🙈🔧 Update config and gitignore.

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ const config = {
   password: process.env.DB_PASS || '',
   dbName: process.env.DB_NAME || 'books_db1',
   tableName: process.env.TABLE_NAME || 'books_table',
-  booksCount: parseInt(process.env.BOOKS_COUNT) || 1000, // - 1e3, 1e5 to long for my mashine;
+  booksCount: parseInt(process.env.BOOKS_COUNT) || 999, // - 1e3, 1e5 to long for my mashine;
+  port: 3000,
 };
 
 module.exports = config;
